94b4eb5e77cda223497d01ee320b52c6aa5db8f1
Francisco Ozuna <ozuna252@hotmail.com>
2020-05-31 15:08:30 -0400

    Added remaining views and some formatting

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -3,14 +3,33 @@ package com.example.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextFavoriteCourse;
+    TextView textViewAfterSubmit;
+    Button buttonClicked;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextFavoriteCourse = (EditText)findViewById(R.id.editTextFavoriteCourse);
+        textViewAfterSubmit = (TextView)findViewById(R.id.textViewAfterSubmit);
+        buttonClicked = (Button)findViewById(R.id.buttonToClick);
     }
 
-    // Testing changes
+    public void onButtonClick(View view) {
+        if(editTextFavoriteCourse.getText().toString().equals("")) {
+            textViewAfterSubmit.setText("");
+        } else {
+            textViewAfterSubmit.setText(editTextFavoriteCourse.getText() + " is fun!");
+        }
+    }
 }
